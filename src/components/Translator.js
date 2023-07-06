e48df3fbdd786ea828b327110ba79fd3def642b1
Teflon <teflon@pop-os.localdomain>
2023-07-06 10:59:51 +0600

    init

---
+++ src/components/Translator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Translator() {
+
+  return (
+    <div>
+        <div>Your Cool Translator</div>
+        <h1>TODO: Add the form here, for text, src lang, and dest lang</h1>
+    </div>
+
+  )
+}
