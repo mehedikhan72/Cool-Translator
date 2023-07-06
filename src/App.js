5b8a82c85cc4c06b42e3878f9a2b409a58d547d8
Teflon <teflon@pop-os.localdomain>
2023-07-06 12:10:09 +0600

    added more todos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Translator from "./components/Translator";
 function App() {
   return (
     <div className="App">
-      <h1>HI MOM</h1>
       <Translator />
     </div>
   );
--- src/components/Translator.js
+++ src/components/Translator.js
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function Translator() {
+  const [text, setText] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
+  const [srcLang, setSrcLang] = useState("");
+  const [targetLang, setTargetLang] = useState("");
+
+  useEffect(() => {
+    // translate the text on a button click(translate button)
+    // store the text in the translatedText state
+    // console.log the new translatedText state
+    // make sure there is no errors and warnings in the console
+    // use fetch api, and the .then method, don't use async await.
+  }, [text, srcLang, targetLang]);
 
   return (
     <div>
-        <div>Your Cool Translator</div>
-        <h1>TODO: Add the form here, for text, src lang, and dest lang</h1>
-        <div>
-          <input type="text" id="textInput" placeholder="Enter text to translate" required></input>
-          <input type="text" id="srcLanguage" placeholder='src' required></input>
-          <input type="text" id="targetLanguage" placeholder='target' required></input>
-          <button type="submit">Translate</button>
-        </div>
-        <p id="translationOutput"></p>
+      <h1>Your Cool Translator</h1>
+      <h3>TODO: Add the form here, for text, src lang, and dest lang</h3>
+      <div>
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+          placeholder="Enter text to translate"
+          required
+        ></input>
+        <input
+          onChange={(e) => setSrcLang(e.target.value)}
+          value={srcLang}
+          type="text"
+          placeholder="src"
+          required
+        ></input>
+        <input
+          value={targetLang}
+          onChange={(e) => setTargetLang(e.target.value)}
+          type="text"
+          placeholder="target"
+          required
+        ></input>
+        <button type="submit">Translate</button>
+      </div>
+      <p id="translationOutput"></p>
     </div>
-
-  )
+  );
 }
